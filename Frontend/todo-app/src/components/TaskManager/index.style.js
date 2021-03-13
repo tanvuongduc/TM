2ae089fc8461c606d9ddcfc33a45eda152d5142f
Luong Manh Dat <luongdat97@gmail.com>
2021-03-13 17:27:45 +0700

    Xây giao diện cho Task Manager 100%

---
+++ Frontend/todo-app/src/components/TaskManager/index.style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const StyleList = styled.div`
+  .list-item {
+    margin-bottom: 10px;
+    border-radius: 3px;
+    height: 40px;
+    vertical-align: baseline;
+  }
+  .input-task {
+    border: 2px solid #c7d8e6;
+  }
+  .btn-action {
+    background-color: #64a7dc;
+    border: 0px; 
+    margin-right: 15px;
+  }
+  .btn-action-danger {
+    background-color: #ff7575;
+  }
+`;
+export default StyleList;
