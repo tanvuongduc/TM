0b2f825b3dd5df07fc8135a73135bedbe4b3f99d
DESKTOP-BBHG6DB\Luat Tien Sinh <vuluat98it@gmail.com>
2021-03-16 18:54:13 +0700

    frontendlogin

--- team3/src/App.js
+++ team3/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import bgr from './Login.png';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundImage:`url(${bgr})`}} className="App">
+ <section className="login" >
+ <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="login__left">
+                <h1 className="login__left__tile">Task Manager</h1>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="login__right">
+                <form action method="post">
+                  <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                  </div>
+                  <div className="input-group mb-3">
+                    <input type="password" className="form-control" placeholder="Password" aria-label="Recipient's password" aria-describedby="basic-addon2" />
+                  </div>
+                  <br />
+                  <br />
+                  <button type="submit" className="btn btn-primary">Login</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+ </section>
+     
+       
+    
+
     </div>
   );
 }
