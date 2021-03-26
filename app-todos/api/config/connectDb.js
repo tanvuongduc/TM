38ec97ac4a5a877b70b6a146228af2be664637d2
anhcachon1@gmail.come <anhcachon1@gmail.com>
2021-03-26 19:35:28 +0700

    connect db

---
+++ app-todos/api/config/connectDb.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+async function connect(){
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log('connect thanh cong')
+  } catch (error) {
+      console.log('ket noi khong thanh cong')
+  }
+}
+module.exports = {connect};
