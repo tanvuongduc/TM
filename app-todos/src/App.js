fb997a0e38f6607b02736d385511d420db44a349
7un7un <dongvanhongpink@gmail.com>
2021-03-29 20:57:37 +0700

    update fetch

--- app-todos/src/App.js
+++ app-todos/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import Login from './components/mainlogin';
+import readAllTodo from './fetch/main';
 
 class App extends Component {
   render() {
     return (
+      <React.StrictMode>
+      <readAllTodo />
+      </React.StrictMode>,
+
       <Login></Login>
     )
   }
