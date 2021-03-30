43c4ccdd9388747493e14da0eb6330fc2c2a6966
Nguyễn Hải Đăng <71062801+nguyenhaidangyq@users.noreply.github.com>
2021-03-30 19:25:33 +0700

    update front-end

--- Front-end/src/Components/TaskManager.js
+++ Front-end/src/Components/TaskManager.js
@@ -5,16 +5,47 @@ import Filter from './Filter';
 import List from './List';
 import InputFrame from './InputFrame';
 class TaskManager extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            user: '',
+            tasks: []
+        }
+    }
+    componentDidMount = async () => {
+        let data = {
+            token: localStorage.getItem('token')
+        }
+        await axios.post(`http://localhost:3000/task`, data)
+            .then(res => {
+                let user = res.data[0].user
+                let tasks = res.data[0].tasks
+                if (user) {
+                    this.setState({
+                        user: user,
+                        tasks: tasks
+                    })
+                }
+                else {
+                    localStorage.setItem('token', '')
+                }
+            })
+            .catch(err => {
+                localStorage.setItem('token', '')
+                console.log(err)
+            })
+    }
     render() {
         return (
             <Fragment>
+
             <div className="container">
                 <Filter/>
                 <div className="left">
                     <List/>
                 </div>
                 <div className="right">
-                    <InputFrame/>
+                   
                 </div>
             </div>
             </Fragment>
