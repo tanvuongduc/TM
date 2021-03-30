f22a067f3cb4de85135b51fc9b97834ad76099b4
huy1405 <n.huy.tq@gmail.com>
2021-03-30 13:28:20 +0700

    Update NavBar.js

--- Front-end/src/Components/NavBar.js
+++ Front-end/src/Components/NavBar.js
@@ -18,11 +18,9 @@ class NavBar extends Component {
                     <b className="h">Task Manager </b>
                     <div className='logout'>
                         <div class="dropdown">
-
                             <button class="dropbtn" > Hi, {this.props.username} &ensp;
                                  <i class="fas fa-sort-down"> </i>
                             </button>
-
                             <div class="dropdown-content">
                                 <a class="fas fa-sign-out-alt" href=" "> &ensp; Logout</a>
                             </div>
