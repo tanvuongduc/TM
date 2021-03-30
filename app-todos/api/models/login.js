c9f4516580ee5a8ab85be1f27136cb7deb164f8a
anhcachon1@gmail.come <anhcachon1@gmail.com>
2021-03-30 19:32:50 +0700

    api login

---
+++ app-todos/api/models/login.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const PostsSchema = mongoose.Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    passWord: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String,
+        default: ""
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+module.exports = mongoose.model('User', PostsSchema);
