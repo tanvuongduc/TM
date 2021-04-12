888ce33fb1e1c1441d0d25cb2860ef0262f56969
ThaiHien170294 <thaihien17294@gmail.com>
2021-04-12 14:39:36 +0700

    hien

--- app-todos/src/components/login-page/login-page.jsx
+++ app-todos/src/components/login-page/login-page.jsx
@@ -11,58 +11,59 @@ export default class Login extends Component {
         super(props)
         this.state = {
             username: '',
-            password: '',
-            link: ''
+            password: ''
         }
+        this.onUsername = this.onUsername.bind(this)
+        this.onPassword = this.onPassword.bind(this)
+        this.onHendleSutmit = this.onHendleSutmit.bind(this)
     }
 
     componentDidMount() {
     }
 
-    Confirm() {
-        if (this.state.username === 'admin' & this.state.password === '123') {
-            this.setState({
-                link: '/work'
-            });
-        }
-        else {
-            alert ('Tên đăng nhập hoặc mật khẩu không đúng!');
-        }
-    }
+    // Confirm() {
+    //     if (this.state.username === 'admin' & this.state.password === '123') {
+    //         this.setState({
+    //             link: '/work'
+    //         });
+    //     }
+    //     else {
+    //         alert('Tên đăng nhập hoặc mật khẩu không đúng!');
+    //     }
+    // }
 
     render() {
         return (
-            <Fragment>
-                <div className='mainlogin'>
-                    <div className='mainleft'>
-                        <h2>Task</h2>
-                        <h2>Manager</h2>
-                    </div>
-                    <div className='mainright'>
-                        <form action=''>
-                            <input type='text' placeholder='Username' value={this.state.username} onChange={(ev) => this.onUsername(ev)} required></input>
-                            <input type='password' placeholder='Password' value={this.state.password} onChange={(ev) => this.onPassword(ev)} required></input>
-                            <br></br>
-                            <NavLink to={this.state.link}>
-                                <button type='submit' onClick={() => this.Confirm()}>LOGIN</button>
-                            </NavLink>
-                        </form>
-                    </div>
+            <div className='mainlogin'>
+                <div className='mainleft'>
+                    <h2>Task</h2>
+                    <h2>Manager</h2>
                 </div>
-            </Fragment>
+                <div className='mainright'>
+                    <form onSubmit={this.onHendleSutmit}>
+                        <input type='text' placeholder='Username' onChange={this.onUsername} required></input>
+                        <input type='password' placeholder='Password' onChange={this.onPassword} required></input>
+                        <br></br>
+                        <button type='submit' >LOGIN</button>
+                    </form>
+                </div>
+            </div>
         )
+    }
+    onHendleSutmit(ev) {
+        ev.preventDefault()
+        console.log(this.state.username);
+        console.log(this.state.password);
     }
     onUsername(ev) {
         this.setState({
             username: ev.target.value
         })
-        // console.log(this.state.username);
     }
     onPassword(ev) {
         this.setState({
             password: ev.target.value
         })
-        // onsole.log(this.state.password);
     }
 }
 
